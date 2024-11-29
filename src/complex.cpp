4aedfe92832e0e918c08e8d6e12e0213deb6a7ba
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-11-30 10:56:12 +1100

    Implemented simple complex number class and main function

---
+++ src/complex.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+#include "../include/Complex.h"
+
+using namespace std;
+
+// Constructor
+Complex::Complex(double r, double i) : real(r), imag(i) {}
+
+// Getter and Setter functions
+double Complex::getReal() const {
+    return real;
+}
+
+double Complex::getImag() const {
+    return imag;
+}
+
+void Complex::setReal(double r) {
+    real = r;
+}
+
+void Complex::setImag(double i) {
+    imag = i;
+}
+
+// Other Operations
+double Complex::magnitude() const {
+    return sqrt(real * real + imag * imag);
+}
+
+Complex Complex::conjugate() const {
+    return Complex(real, -imag);
+}
+
+// Display
+void Complex::print_cartesian() const {
+    cout << "complex number in cartesian form\n";
+}
