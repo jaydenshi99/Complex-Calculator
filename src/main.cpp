bfbf17c6a2c2d709d5609dd72ccedfeddaf2fe91
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-11-30 12:15:16 +1100

    mandlebrot set in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,20 @@
 using namespace std;
 
 int main (void) {
-    const double inc = 0.05;
+    const double inc = 0.04;
     for (double y = 1; y >= -1; y -= inc) {
-        for (double x = -2; x <= -1; x += inc) {
+        for (double x = -2; x <= 1; x += inc/2) {
             Complex c = Complex(x, y);
             Complex z = Complex(x, y);
 
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 100; i++) {
                 z = z * z + c;
             }
 
-            if (fabs(z.getImag()) == INFINITY || z.getReal() == INFINITY) {
+            if (
+                fabs(z.getReal()) == INFINITY || z.getImag() == INFINITY ||
+                isnan(z.getReal()) || isnan(z.getImag())
+            ) {
                 cout << " ";
             } else {
                 cout << "#";
