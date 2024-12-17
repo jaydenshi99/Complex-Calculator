d63fd3dc7f925b040ce8fd5d7ca09f2b2dacad29
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-12-17 16:00:51 +1100

    revert main

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,6 @@
 #include "../include/functions.h"
 
 int main (void) {
-    Complex z = Complex(1, 2);
-    cexp(z).display_cartesian();
-    csin(z).display_cartesian();
-    ccos(z).display_cartesian();
-    ctan(z).display_cartesian();
-    clogp(z).display_cartesian();
-    csinh(z).display_cartesian();
-    ccosh(z).display_cartesian();
-    ctanh(z).display_cartesian();
-    // Calculator calc;
-    // calc.initialise();
+    Calculator calc;
+    calc.initialise();
 }
