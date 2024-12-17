55d8383543b30b4f8fcc88506b9224319874fd37
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-12-17 15:22:02 +1100

    update main for testing

--- include/functions.h
+++ include/functions.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include "complex.h";
+#include "complex.h"
 
 // Exponential
 Complex cExp(Complex z);
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include "../include/complex.h"
 #include "../include/other.h"
 #include "../include/calculator.h"
+#include "../include/functions.h"
 
 int main (void) {
-    Calculator calc;
-    calc.initialise();
+    Complex z = Complex(1, 2);
+    cExp(z).display_cartesian();
+    cSin(z).display_cartesian();
+    cCos(z).display_cartesian();
+    cTan(z).display_cartesian();
+    // Calculator calc;
+    // calc.initialise();
 }
